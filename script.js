bde89560732a0e6ae6088b71b528a530208a85be
rafay45 <rafayy579@gmail.com>
2025-06-03 16:59:52 +0500

    🎚️🎢Completed

--- script.js
+++ script.js
@@ -2,22 +2,31 @@ let prev = document.querySelector('.prev');
 let next = document.querySelector('.next');
 let slides = document.querySelectorAll('.slideImg');
 let allSlides = document.querySelector('.allSlides');
+let heading = document.querySelectorAll('.heading');
 
 let currentIndex = 0;
 
 function show(index) {
     let totalSlides = slides.length;
-    if(index >= totalSlides) currentIndex = 0
-    else if(index < 0) currentIndex = totalSlides - 1
+    let totalText = heading.length;
+    if (index >= totalSlides && index >= totalText) {
+        currentIndex = 0
+    }
+    if (index < 0 ) {
+        currentIndex = totalSlides - 1;
+        currentIndex = totalText - 1;
+    }
 
     const offset = -currentIndex * 600;
-   document.querySelector('.allSlides').style.transform = `translateX(${offset}px)`;
+    const offsetText = -currentIndex * 200;
+    document.querySelector('.allHeadings').style.transform = `translateX(${offsetText}px)`;
+    document.querySelector('.allSlides').style.transform = `translateX(${offset}px)`;
 }
 
 next.addEventListener('click', () => {
     currentIndex++;
     show(currentIndex);
-}); 
+});
 prev.addEventListener('click', () => {
     currentIndex--;
     show(currentIndex);
